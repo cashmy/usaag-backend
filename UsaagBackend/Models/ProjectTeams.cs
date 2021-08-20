864ca25265c64ef7e3b9d26f9938c55bddae6918
cashmy <cmyers880@gmail.com>
2021-08-20 10:18:02 -0500

    Removed TeamAssignments Model - Merged into Project Teams

--- UsaagBackend/Models/ProjectTeams.cs
+++ UsaagBackend/Models/ProjectTeams.cs
@@ -7,14 +7,14 @@ namespace UsaagBackend.Models
     public class ProjectTeams
     {
         [Key, Column(Order = 1)]
-        [ForeignKey("Projects")]
-        public int ProjectId { get; set; }
-        public Projects Projects { get; set; }
-
-        [Key, Column(Order = 2)]
         [ForeignKey("Teams")]
         public int TeamId { get; set; }
         public Teams Teams { get; set; }
+
+        [Key, Column(Order = 2)]
+        [ForeignKey("Students")]
+        public int StudentId { get; set; }
+        public Students Students { get; set; }
 
         // ** Future use attributes
         public float TotalPointsScored { get; set; }
