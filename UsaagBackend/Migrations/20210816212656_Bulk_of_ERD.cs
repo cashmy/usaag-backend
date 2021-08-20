dc026c54bee62eea4d10e3c50d55e116eb73b5da
cashmy <cmyers880@gmail.com>
2021-08-20 09:37:50 -0500

    Initial Setup

---
+++ UsaagBackend/Migrations/20210816212656_Bulk_of_ERD.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UsaagBackend.Migrations
+{
+    public partial class Bulk_of_ERD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CohortId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalPoints = table.Column<float>(type: "real", nullable: false),
+                    TotalWeightedPoints = table.Column<float>(type: "real", nullable: false),
+                    DateAssigned = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateDue = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Cohorts_CohortId",
+                        column: x => x.CohortId,
+                        principalTable: "Cohorts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InstructorSlackChannel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CohortId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Cohorts_CohortId",
+                        column: x => x.CohortId,
+                        principalTable: "Cohorts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Group = table.Column<bool>(type: "bit", nullable: false),
+                    AutoGenName = table.Column<bool>(type: "bit", nullable: false),
+                    GitHubRepository = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TeamTrelloBoard = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemplateHeader",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Goal = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    SpecialNotes = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    TotalPoints = table.Column<float>(type: "real", nullable: false),
+                    TotalWeightedPoints = table.Column<float>(type: "real", nullable: false),
+                    VersionMain = table.Column<int>(type: "int", nullable: false),
+                    VersionMinor = table.Column<int>(type: "int", nullable: false),
+                    VersionSub = table.Column<int>(type: "int", nullable: false),
+                    NotionScript = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateHeader", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectDetails",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    PointValue = table.Column<int>(type: "int", nullable: false),
+                    BonusStatus = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    PercentComplete = table.Column<int>(type: "int", nullable: false),
+                    GradingScript = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PointsScored = table.Column<float>(type: "real", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectDetails", x => new { x.ProjectId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ProjectDetails_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectDetails_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemplateDetail",
+                columns: table => new
+                {
+                    HeaderId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    PointValue = table.Column<int>(type: "int", nullable: false),
+                    BonusStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateDetail", x => new { x.HeaderId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_TemplateDetail_TemplateHeader_HeaderId",
+                        column: x => x.HeaderId,
+                        principalTable: "TemplateHeader",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectDetails_StudentId",
+                table: "ProjectDetails",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CohortId",
+                table: "Projects",
+                column: "CohortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CohortId",
+                table: "Students",
+                column: "CohortId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectDetails");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "TemplateDetail");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "TemplateHeader");
+        }
+    }
+}
