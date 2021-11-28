24da390117eb87df0fe21b0dbc04c13c33a31954
Cash Myers <cmyers880@gmail.com>
2021-11-28 16:11:06 -0600

    Created Curr Type Model and Controller

---
+++ UsaagBackend/Controllers/CurriculumTypesController.cs
@@ -0,0 +1,129 @@
+﻿using UsaagBackend.Data;
+using UsaagBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UsaagBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurriculumTypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CurriculumTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // ***** GET ALL CurriculumTypes *****
+        // <baseurl/api/curriculumTypes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var curriculumTypes = _context.CurriculumTypes.ToList();
+            if (curriculumTypes == null)
+            {
+                return NotFound();
+            }
+            return Ok(curriculumTypes);
+        }
+
+        // ***** GET ALL Curriculum Types by Archive Status *****
+        // <baseurl/api/curriculumTypes/archive/
+        [HttpGet("archive/{Status}")]
+        public IActionResult GetStatus(bool Status)
+        {
+            var curriculumTypes = _context.CurriculumTypes
+                .Where(c => c.Archived == Status)
+                .ToList();
+            if (curriculumTypes == null)
+            {
+                return NotFound();
+            }
+            return Ok(curriculumTypes);
+        }
+
+
+        // ***** GET A Curriculum Type by ID *****
+        // <baseurl/api/curriculumTypes
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            var curriculumType = _context.CurriculumTypes
+                .Where(c => c.Id == Id)
+                .SingleOrDefault();
+
+            return Ok(curriculumType);
+
+        }
+
+        // ***** ADD A Curriculum Type *****
+        // POST /api/curriculumTypes
+        [HttpPost]
+        public IActionResult Post([FromBody] CurriculumType value)
+        {
+            _context.CurriculumTypes.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        // ***** UPDATE A Curriculum Type *****
+        // PUT /api/curriculumTypes
+        [HttpPut("{Id}")]
+        public IActionResult Put(int Id, [FromBody] CurriculumType value)
+        {
+            var curriculumType = _context.CurriculumTypes.Where(c => c.Id == Id).SingleOrDefault();
+            if (curriculumType == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+            curriculumType.Name = value.Name;
+            curriculumType.Abbreviation = value.Abbreviation;
+            curriculumType.ChipColor = value.ChipColor;
+            curriculumType.TextColor = value.TextColor;
+    
+            _context.CurriculumTypes.Update(curriculumType);
+            _context.SaveChanges();
+            return StatusCode(201, curriculumType);
+        }
+
+        // ***** PATCH A Curriculum Type's Archived status*****
+        // PATCH /api/curriculumTypes
+        [HttpPatch("{Id}")]
+        public IActionResult Patch(int Id, [FromBody] CurriculumType value)
+        {
+            var curriculumType = _context.CurriculumTypes.Where(c => c.Id == Id).SingleOrDefault();
+            if (curriculumType == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+
+            curriculumType.Archived = value.Archived;
+
+            _context.CurriculumTypes.Update(curriculumType);
+            _context.SaveChanges();
+            return StatusCode(201, curriculumType);
+        }
+
+        // ***** DELETE A Curriculum Type *****
+        // **** NOT TO BE USED IN GENERAL SETTING 
+        // DELETE /api/curriculumType
+        [HttpDelete("{Id}"), Authorize]
+        public IActionResult Delete(int Id)
+        {
+
+            var curriculumType = _context.CurriculumTypes.Where(c => c.Id == Id).SingleOrDefault();
+            if (curriculumType == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+
+            _context.CurriculumTypes.Remove(curriculumType);
+            _context.SaveChanges();
+            return StatusCode(204, curriculumType);
+        }
+    }
+}
+
---
+++ UsaagBackend/Migrations/20211128154641_curriculumType.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UsaagBackend.Migrations
+{
+    public partial class curriculumType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurriculumTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Abbreviation = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: true),
+                    Archived = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    ChipColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TextColor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurriculumTypes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CurriculumTypes",
+                columns: new[] { "Id", "Abbreviation", "Archived", "ChipColor", "Name", "TextColor" },
+                values: new object[,]
+                {
+                    { 1, "Note", false, "gray", "Note", "white" },
+                    { 2, "Lect", false, "yellow", "Lecture", "black" },
+                    { 3, "Demo", false, "orange", "Demo", "black" },
+                    { 4, "WS", false, "cyan", "Worksheet", "black" },
+                    { 5, "MLab", false, "blue", "Mini-Lab", "white" },
+                    { 6, "Lab", false, "darkblue", "Lab", "white" },
+                    { 7, "Proj", false, "purple", "Project", "white" },
+                    { 8, "GCap", false, "lime", "Group Capstone", "black" },
+                    { 9, "ICap", false, "green", "Individual Capstone", "white" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurriculumTypes");
+        }
+    }
+}
